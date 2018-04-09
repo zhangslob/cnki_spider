47f3d6bee3586cd8afb2c90dc48e64ce40909ece
zhangslob <zhangslob@gmail.com>
2018-04-09 23:46:05 +0800

    test

--- cnki_spider/spiders/cnki.py
+++ cnki_spider/spiders/cnki.py
@@ -8,22 +8,22 @@ from time import time
 class CnkiSpider(scrapy.Spider):
     name = 'cnki'
     allowed_domains = ['cnki.net']
-    base_url = 'http://kns.cnki.net'
-    id_url = 'http://kns.cnki.net/KCMS/detail/detail.aspx?dbcode=CJFQ&dbname=CJFDTEMP&filename={}'
+    base_url = 'http://kns.cnki.net/kns/brief/default_result.aspx'
+    cookie_url = 'http://kns.cnki.net/KRS/scripts/Recommend.js'
+    page_url = 'http://kns.cnki.net/kns/brief/brief.aspx?curpage=2&RecordsPerPage=20&QueryID=7&ID=&turnpage=1&tpagemode=L&dbPrefix=SCDB&Fields=&DisplayMode=listmode&PageName=ASP.brief_default_result_aspx'
+
+
     # start_urls = ['http://kns.cnki.net/kns/brief/default_result.aspx']
     t = int(time())
     headers = {
-    'ip': "127.0.0.1",
-    'sign': "4f07c0eb90f194803aeee9783abe781b5e3da611",
-    'location': "0,0",
-    'app_id': "cnki_cajcloud",
-    'did': "{123456}",
-    'timestamp': t,
-    'host': "api2.cnki.net",
-    'connection': "Keep-Alive",
-    'accept-encoding': "gzip",
-    'user-agent': "okhttp/2.7.5",
-    'cache-control': "no-cache",
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Host': 'kns.cnki.net',
+        'Upgrade-Insecure-Requests': 1,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
     }
     custom_settings = {
         'DEFAULT_REQUEST_HEADERS': headers
@@ -31,21 +31,32 @@ class CnkiSpider(scrapy.Spider):
 
     def start_requests(self):
 
-        url = 'http://api2.cnki.net/v20/api/db/Literature%7BCJFD%2CCDFD%2CCMFD%2CCPFD%7D?fields=Source,Contributor,Creator,Title,UpdateDate,SubjectCode,SubjectSubColumnCode,CitedTimes,DownloadedTimes,Date,Summary,IsPublishAhead,Year,Issue,CoreJournal,SourceCode,FILETYPE&query=Subject+like+XLS%28%27%E6%B5%99%E6%B1%9F+%E5%88%9B%E6%96%B0+%E5%8C%BA%E5%9F%9F%27%29+or+Title+eq+XLS%28%27%E6%B5%99%E6%B1%9F+%E5%88%9B%E6%96%B0+%E5%8C%BA%E5%9F%9F%27%29+and+Date+Ge+%272013-04-09%27+&group=&order=Date+desc&start=30&length=15'
+        url = self.base_url
         yield scrapy.Request(url, meta={'cookiejar': 1})
 
     def parse(self, response):
-        data = json.loads(response.text)['Rows']
-        for i in data:
-            id = i.get('Id', '')
-            url = self.id_url.format(id)
-            yield scrapy.Request(url, callback=self.detail)
+        url = self.cookie_url
+        yield scrapy.Request(url, meta={'cookiejar': response.meta['cookiejar']},
+                             callback=self.parse_cookie)
 
-    def detail(self, response):
-        data = dict()
-        data['url'] = response.url
-        data['title'] = ''.join(response.xpath('//*[@id="mainArea"]/div[3]/div[1]/h2/text()').extract())
-        data['author'] = ''.join(response.xpath('//*[@id="mainArea"]/div[3]/div[1]/div[1]/span/a/text()').extract())
+    def parse_cookie(self, response):
+        url = self.page_url
+        yield scrapy.Request(url, meta={'cookiejar': response.meta['cookiejar']},
+                             callback=self.parse_url)
 
-        yield data
+    def parse_url(self, response):
+        pass
+    #     data = json.loads(response.text)['Rows']
+    #     for i in data:
+    #         id = i.get('Id', '')
+    #         url = self.id_url.format(id)
+    #         yield scrapy.Request(url, callback=self.detail)
+    #
+    # def detail(self, response):
+    #     data = dict()
+    #     data['url'] = response.url
+    #     data['title'] = ''.join(response.xpath('//*[@id="mainArea"]/div[3]/div[1]/h2/text()').extract())
+    #     data['author'] = ''.join(response.xpath('//*[@id="mainArea"]/div[3]/div[1]/div[1]/span/a/text()').extract())
+    #
+    #     yield data
 
